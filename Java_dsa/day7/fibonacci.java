bca50f74a0ae558a7f281c2a05647403b767af6b
JISHNU <166325684+jishnuGG@users.noreply.github.com>
2024-09-10 09:55:51 +0530

    Update fibonacci.java

--- Java_dsa/day7/fibonacci.java
+++ Java_dsa/day7/fibonacci.java
@@ -1,19 +1,23 @@
 import java.util.Scanner;
 
 public class fibonacci {
-    public static void Fibonacci(int n,int k)
+    public static int Fibonacci(int n)
     {
-        if(n<=k)
+        if(n==1)
         {
-          System.out.println(k);
-          Fibonacci(n-1, k);
+            return 1;
         }
+        else if(n==0)
+        {
+            return 0;
+        }
+        return Fibonacci(n-1)+Fibonacci(n-2);
     }
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
-        int k=sc.nextInt();
-        Fibonacci(n, k);
+       
+        System.out.println( Fibonacci(n));
         sc.close();
     }
 }
